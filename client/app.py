f445fb95d8169ebf71643e11f790275708d2cf6d
cwwojin <cwwojin@gmail.com>
2024-09-01 17:53:44 +0900

    feat : dockerize streamlit client

--- client/app.py
+++ client/app.py
@@ -124,8 +124,6 @@ if platform.system() == "Darwin":  # 맥
 elif platform.system() == "Windows":  # 윈도우
     plt.rc("font", family="Malgun Gothic")
 elif platform.system() == "Linux":  # 리눅스 (구글 콜랩)
-    #!wget "https://www.wfonts.com/download/data/2016/06/13/malgun-gothic/malgun.ttf"
-    #!mv malgun.ttf /usr/share/fonts/truetype/
     # import matplotlib.font_manager as fm
     # fm._rebuild()
     plt.rc("font", family="Malgun Gothic")
