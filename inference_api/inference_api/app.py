1159525bca0368e65e7d46871ac13b19cd884b51
cwwojin <cwwojin@gmail.com>
2024-09-02 12:59:38 +0900

    feat : API - 24hour cache limit, force_reload option

--- inference_api/inference_api/app.py
+++ inference_api/inference_api/app.py
@@ -7,7 +7,7 @@ from starlette.status import HTTP_200_OK
 from inference_api.config import config
 from inference_api.models import InferenceDto, InferenceResult
 from inference_api.services import inferenceService
-from inference_api.utils import ApiKeyGuard
+from inference_api.utils import ApiKeyGuard, parse_boolean
 
 app = FastAPI()
 
@@ -22,7 +22,10 @@ def health_check():
 def get_inference_result(
     inferenceDto: InferenceDto,
     model_name: str = None,
+    force_reload: str = None,
     api_key: APIKey = Depends(ApiKeyGuard),
 ) -> List[InferenceResult]:
     """Run model inference."""
-    return inferenceService.get_inference_results(inferenceDto, model_name)
+    return inferenceService.get_inference_results(
+        inferenceDto, model_name, parse_boolean(force_reload)
+    )
